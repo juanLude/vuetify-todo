22a79ac5d914bc111013a215795af883d468cb0e
Flo Wil <jmludevid@hotmail.com>
2021-12-19 15:24:51 +1100

    Initial Commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-16">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolor molestiae error odit dolorum, repudiandae iure asperiores ratione corrupti debitis quibusdam, reprehenderit impedit maiores voluptate assumenda illum a mollitia maxime.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolor molestiae error odit dolorum, repudiandae iure asperiores ratione corrupti debitis quibusdam, reprehenderit impedit maiores voluptate assumenda illum a mollitia maxime.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolor molestiae error odit dolorum, repudiandae iure asperiores ratione corrupti debitis quibusdam, reprehenderit impedit maiores voluptate assumenda illum a mollitia maxime.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolor molestiae error odit dolorum, repudiandae iure asperiores ratione corrupti debitis quibusdam, reprehenderit impedit maiores voluptate assumenda illum a mollitia maxime.</p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    
+    
+
+    <list-tasks 
+      v-if="$store.state.tasks.length"
+    />
+    
+  <no-tasks v-else />
+  <button-done-sorting 
+    v-if="$store.state.sorting"/>
+  </div>
+</template>
+
+<script>
+
+import ListTasks from '@/components/Todo/ListTasks'
+import NoTasks from '@/components/Todo/NoTasks'
+import ButtonDoneSorting from '@/components/Todo/ButtonDoneSorting'
+export default {
+  name: 'Home',
+   components: {
+      ListTasks, NoTasks, ButtonDoneSorting
+      
+    }
+  
+}
+</script>
+
+<style lang="sass">
+  .no-tasks
+    position: absolute
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+    opacity: 0.5
+</style>
